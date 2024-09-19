c488aac0d041a9c428fc51d8c07e86fbbb0d4ec1
FranklinSolano <100084667+FranklinSolano@users.noreply.github.com>
2024-09-19 14:08:28 -0300

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheckFitnessPersonal
+//
+//  Created by Franklin  Stilhano Solano on 19/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
